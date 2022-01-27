8224be2c41d38704215c43722fe0469302a534a0
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-27 13:35:06 +0200

    add index and show functions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationsController < ApplicationController
   def create
     application = Application.create(name: params[:name], chats_count: 0)
-    msg = {success: true, token: application.token}
-    puts msg
+    msg = { success: true, token: application.token }
     render json: msg
+  end
+
+  def index
+    applications = Application.select('name, token, chats_count').all
+    render json: { success: true, data: applications }
+  end
+
+  def show
+    application = Application.select('name, token, chats_count')
+      .where(token: params[:token])
+    if application.blank?
+      render json: { success: false, message: "application not found" }, status: 404
+    else  
+      render json: { success: true, data: application }
+    end
   end
 end
