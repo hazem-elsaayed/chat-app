4575b035e46c22244d34173e8092f2c844b34a16
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-27 12:43:02 +0200

    add create function to application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationsController < ApplicationController
+  def create
+    application = Application.create(name: params[:name], chats_count: 0)
+    msg = {success: true, token: application.token}
+    puts msg
+    render json: msg
+  end
 end
