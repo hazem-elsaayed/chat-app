de7a00a8655566d20b88e7a0f073a448b172b2b5
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-27 12:43:26 +0200

    add before create hook to generate a unique token

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,10 @@
 class Application < ApplicationRecord
   has_many :chats, class_name: "chat", foreign_key: "application_id"
+  before_create :generate_token
+  def generate_token(length=10)
+    loop do
+      token = self.token = SecureRandom.urlsafe_base64(length, false)
+      break token unless self.class.exists?(token: token)
+    end
+  end
 end
