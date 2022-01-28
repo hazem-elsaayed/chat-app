59ad64288c0d842871ef8e28217c5c186faa5b66
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-01-28 14:26:46 +0200

    add function to handle create new chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,11 @@
 class ChatsController < ApplicationController
+  def create
+    return render json: { success: false, message: 'wrong params' }, status: 400 if params[:name].blank?
+    last_chat = Chat.joins('JOIN applications ON applications.id = chats.application_id').where('applications.token' => params[:token]).last
+    chat_number = last_chat.blank? ? 1 : last_chat.chat_number + 1
+    new_chat = Chat.create(name: params[:name], chat_number: chat_number, messages_count: 0)
+    render json: { success: true, chat_number: new_chat.chat_number }
+  end
+
+  
 end
