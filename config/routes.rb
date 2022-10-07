72f5ab1142de5b21e58dd2d19bf72c56ac4e2af9
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2022-10-07 20:05:56 +0200

    clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  post 'application', to: 'applications#create'
-  get 'applications', to: 'applications#index'
-  get 'applications/:token', to: 'applications#show'
-  patch 'applications/:token', to: 'applications#update'
 
-  post 'applications/:token/chat', to: 'chats#create'
-  get 'applications/:token/chats', to: 'chats#index'
-  get 'applications/:token/chats/:chat_number', to: 'chats#show'
-  patch 'applications/:token/chats/:chat_number', to: 'chats#update'
-
-  post 'applications/:token/chats/:chat_number/message', to: 'messages#create'
-  get 'applications/:token/chats/:chat_number/messages', to: 'messages#index'
-  get 'applications/:token/chats/:chat_number/messages/search', to: 'messages#search'
-  get 'applications/:token/chats/:chat_number/messages/:message_number', to: 'messages#show'
-  patch 'applications/:token/chats/:chat_number/messages/:message_number', to: 'messages#update'
+  resources :applications, only: [:index, :show, :update, :create], param: :token do
+    resources :chats, only: [:index, :show, :update, :create], param: :number do
+      resources :messages, only: [:index, :show, :update, :create], param: :message_number do
+        get 'search', on: :collection
+      end
+    end
+  end 
 
 end
